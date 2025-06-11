84e20764f2d504534133951baee0c0c7956cc572
Christian Dickreuter <christian.dickreuter@gmail.com>
2025-06-11 20:46:53 +0200

    Add Vuetify support and custom theme to the project

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,20 +1,17 @@
 <template>
     <h1 class="text-3xl">Unsere Lieblings-Restaurants</h1>
-    <ul>
-        <li v-for="restaurant in restaurants" key="restaurnat.id">
-            {{ restaurant.name }} - {{ restaurant.cuisine }}
-        </li>
-    </ul>
+    <v-list :items="restaurants" item-title="name"></v-list>
+
+    <v-btn color="primary">Button </v-btn>
 </template>
 
 <script setup lang="ts">
-    defineProps<{
-        restaurants: {
-            id: number;
-            name: string;
-            cuisine: string;
-            url: string;
-        }[];
-    }>();
-
+defineProps<{
+    restaurants: {
+        id: number;
+        name: string;
+        cuisine: string;
+        url: string;
+    }[];
+}>();
 </script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,22 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import CustomTheme from './Theme/CustomTheme';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+        defaultTheme: "CustomTheme",
+        themes: {
+            CustomTheme,
+        },
+    },
+})
 
 createInertiaApp({
   resolve: name => {
@@ -9,6 +26,7 @@ createInertiaApp({
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
+      .use(vuetify)
       .mount(el)
   },
 })
